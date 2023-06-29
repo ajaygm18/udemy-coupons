94c34d501f82a058977c6c2055911017f85ade16
AJAY GONHALMATH <72427790+ajaygm18@users.noreply.github.com>
2023-06-30 04:16:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,22 @@ breaker = False
 
 def bsdk(num):
     global breaker, u_link
-    rs = requests.get('https://www.udemyfreebies.com/free-udemy-courses/'+str(num))
+    headers = {
+        'authority': 'www.udemyfreebies.com',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7,kn;q=0.6',
+        'cache-control': 'max-age=0',
+        'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'none',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+    }
+    rs = requests.get('https://www.udemyfreebies.com/free-udemy-courses/'+str(num), headers=headers)
     soup = BeautifulSoup(rs.text, 'lxml')
 
     cpns = soup.select('.col-md-4.col-sm-6 .theme-block')
@@ -37,7 +52,7 @@ for il in range(1, 15):
     if breaker == True:
         break
     bsdk(il)
-print(u_link)
+
 if len(u_link) > 1:
     with open('results.tsv', 'w', encoding='utf-8') as file:
         writer = csv.writer(file, lineterminator='\n', delimiter='\t')
